babfa19a3ac4a7c4a895aa2adcb118fcdea097b6
Jurij Zakharov <amourphys@gmail.com>
2021-12-13 11:52:39 +0200

    create new directory:Mixins and inserting into file:toggleMixins.js

--- src/Components/UI/MyDialog.vue
+++ src/Components/UI/MyDialog.vue
@@ -7,19 +7,11 @@
 </template>
 
 <script>
+import toggleMixin from "@/Mixins/toggleMixin.js";
+
 export default {
   name: "my-dialog",
-  props: {
-    show: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  methods: {
-    hideDialog() {
-      this.$emit("update:show", false);
-    },
-  },
+  mixins: [toggleMixin],
 };
 </script>
 
